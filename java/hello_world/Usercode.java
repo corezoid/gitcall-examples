4d62ec30932e498f1e7d9200e843b00c86eec2b0
Alexander Kozienko <a.s.kozienko@gmail.com>
2021-02-08 10:53:57 +0200

    update handler name

--- java/hello_world/Usercode.java
+++ java/hello_world/Usercode.java
@@ -1,10 +1,10 @@
 // fq-class name com.corezoid.usercode.Usercode is mandatory
 package com.corezoid.usercode;
 
-import com.corezoid.gitcall.runner.api.IUsercode;
+import com.corezoid.gitcall.runner.api.UsercodeHandler;
 import java.util.Map;
 
-public class Usercode implements IUsercode<Map<String, String>, Map<String, String>> {
+public class Usercode implements UsercodeHandler<Map<String, String>, Map<String, String>> {
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
         data.put("hello", "Hello world!");
--- java/http_request/usercode/src/main/java/com/corezoid/usercode/Usercode.java
+++ java/http_request/usercode/src/main/java/com/corezoid/usercode/Usercode.java
@@ -1,13 +1,13 @@
 // fq-class name com.corezoid.usercode.Usercode is mandatory
 package com.corezoid.usercode;
 
-import com.corezoid.gitcall.runner.api.IUsercode;
+import com.corezoid.gitcall.runner.api.UsercodeHandler;
 import java.util.Map;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class Usercode implements IUsercode<Map<String, String>, Map<String, String>> {
+public class Usercode implements UsercodeHandler<Map<String, String>, Map<String, String>> {
 
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
