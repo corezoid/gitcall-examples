628d14b0e1a0dd912a1adba069e0b17e0695c984
Dmitry Honcharov <dmitriy.goncharov@corezoid.com>
2024-08-02 19:24:28 +0300

    added python logs

--- dockerfile/python/src/main.py
+++ dockerfile/python/src/main.py
@@ -1,15 +1,23 @@
+import time
+import logging
 from fastapi import FastAPI, Body
 from usercode import handle
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
 @app.post("/")
-def hello(req = Body()):
+def request(req = Body()):
     jsonrpc = req["jsonrpc"]
     id = req["id"]
     params = req["params"]
     try:
+        logger.info("[req] time=%d id=%s" % (round(time.time() * 1000), id))
         result = handle(params)
+        logger.info("[res] time=%d id=%s" % (round(time.time() * 1000), id))
         return {"jsonrpc": jsonrpc, "id": id, "result": result}
     except Exception as err:
+        logger.info("[res] time=%d id=%s error=%s" % (round(time.time() * 1000), id, str(err)))
         return {"jsonrpc": jsonrpc, "id": id, "error": {"code": 1, "message": str(err)}}
