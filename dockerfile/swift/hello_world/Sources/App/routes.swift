04cdb73a015ed56ba65c295cd915b44bf811f761
Dmitry Honcharov <dmitriy.goncharov@corezoid.com>
2024-08-14 18:01:43 +0300

    fix swift logs

--- dockerfile/swift/hello_world/Sources/App/routes.swift
+++ dockerfile/swift/hello_world/Sources/App/routes.swift
@@ -3,20 +3,20 @@ import Vapor
 func routes(_ app: Application) throws {
     app.post { req async throws -> String in
         let request = try req.content.decode(Request.self)
-        print("[req] time=\(Date().millisecondsSince1970) id=\(request.id)")
+        req.logger.info("[req] time=\(Date().millisecondsSince1970) id=\(request.id)")
         do {
             let result = try usercode(request.params)
             let resp = Response(id: request.id, jsonrpc: request.jsonrpc, result: result, error: nil)
             let jsonEncoder = JSONEncoder()
             let jsonResultData = try? jsonEncoder.encode(resp)
-            print("[res] time=\(Date().millisecondsSince1970) id=\(request.id)")
+            req.logger.info("[res] time=\(Date().millisecondsSince1970) id=\(request.id)")
             return String(data: jsonResultData!, encoding: .utf8) ?? ""
         } catch let error {
             let message = "\(error)"
             let resp = Response(id: request.id, jsonrpc: request.jsonrpc, result: nil, error: ResponseError(code: 1, message: message))
             let jsonEncoder = JSONEncoder()
             let jsonResultData = try? jsonEncoder.encode(resp)
-            print("[res] time=\(Date().millisecondsSince1970) id=\(request.id) error=\(error)")
+            req.logger.info("[res] time=\(Date().millisecondsSince1970) id=\(request.id) error=\(error)")
             return String(data: jsonResultData!, encoding: .utf8) ?? ""
         }    
     }
